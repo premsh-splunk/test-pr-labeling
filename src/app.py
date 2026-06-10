666a7dd82f15e0308a537de6d25f94f2e9d27a22
premsh-splunk <premsh@splunk.com>
2026-06-10 10:41:12 +0530

    feat: add Acme SDK app entry point

---
+++ src/app.py
@@ -0,0 +1,10 @@
+from soar_sdk.app import App
+
+app = App(
+    name="Acme SDK App",
+    app_type="sandbox",
+    product_vendor="Acme",
+    product_name="Acme SDK App",
+    publisher="Acme",
+    appid="99999999-8888-7777-6666-555555555555",
+)
